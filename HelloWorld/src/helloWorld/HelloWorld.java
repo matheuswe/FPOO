bdf12b11b3fddeeca988edff091aba99d663f1a5
Matheus <matheusweber@alunos.utfpr.edu.br>
2025-05-27 22:34:20 -0300

    Projeto Maven tic-tac-toe criado

---
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package helloWorld;
+
+public class HelloWorld {
+	
+	public static void main (String []args) {		
+		
+		if(args.length 	== 0)
+			System.out.println("Use: Java helloWorld.HelloWorld \"Matheus Weber\" ");
+		
+			System.out.println("Olá"+ args[0]);
+	}
+}
